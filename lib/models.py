82c509c3e06598a5a1c383e9d0352c8e49469c42
JVercout <jerome.vercoutere@gmail.com>
2017-01-06 14:11:18 +0100

    Device Model

--- lib/models.py
+++ lib/models.py
@@ -12,4 +12,28 @@ class BaseModel(Model):
         database = db
 
 
+class Device(BaseModel):
+    token = CharField()
 
+    def __repr__(self):
+        return "<Device('%s')>" % (self.id)
+
+
+class DeviceParameter(BaseModel):
+    device_id = ForeignKeyField(Device, related_name='parameters', index=True)
+    identifier = CharField(null=False, max_length=32)
+    value_lsp = TextField(null=False)
+    value_sent = TextField(null=False)
+    value_device = TextField(null=False)
+
+    def __repr__(self):
+        return "<DeviceParameter('%s')>" % (self.identifier)
+
+
+class Workflow(BaseModel):
+    device_id = ForeignKeyField(Device, related_name='workflows', index=True)
+    workflow_type = CharField(null=False)
+    config_json = TextField()
+
+    def __repr__(self):
+        return "<Workflow('%s', '%s')>" % (self.device_id, self.workflow_type)
